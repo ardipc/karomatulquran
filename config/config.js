098f461083b1f75e7a9f685165039f6dc01c6e53
ahmad ardiansyah <ardiansyah3ber@gmail.com>
2021-02-18 09:34:24 +0700

    update production

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "karomatulquran",
+    "username": "karomatu_akses",
+    "password": "karomatulquran123",
+    "database": "karomatu_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
--- config/envi.js
+++ config/envi.js
@@ -1,14 +1,14 @@
 module.exports = {
-	'app_url': 'http://localhost:3000',
-	'app_url_back': 'http://localhost:3000/administrator',
+	'app_url': 'https://karomatulquran.com',
+	'app_url_back': 'https://karomatulquran.com/administrator',
 	'app_port': 3000,
 	'app_env': 'development',
 
-	'db_host': '127.0.0.1',
+	'db_host': 'localhost',
 	'db_port': 3306,
-	'db_user': 'root',
-	'db_pass': '',
-	'db_name': 'karomatulquran',
+	'db_user': 'karomatu_akses',
+	'db_pass': 'karomatulquran123',
+	'db_name': 'karomatu_db',
 
 	'app_tinymce_key': 'j18ccoizrbdzpcunfqk7dugx72d7u9kfwls7xlpxg7m21mb5'
 }
